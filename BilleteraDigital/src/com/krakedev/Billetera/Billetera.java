babe7134f3c349970c2adbae5dc4c27bbb3c8af7
kerveros12v <kerveros12v@aol.com>
2026-03-26 19:52:05 -0500

    feal: add constructor

--- BilleteraDigital/src/com/krakedev/Billetera/Billetera.java
+++ BilleteraDigital/src/com/krakedev/Billetera/Billetera.java
@@ -2,4 +2,10 @@ public class Billetera {
 private String propietario,numeroCuenta;
 private double saldo;
 private boolean activa;
+public Billetera(String propietario, String numeroCuenta, double saldoInicial){
+    this.propietario=propietario;
+    this.numeroCuenta=numeroCuenta;
+    this.saldo=saldoInicial;
+    this.activa=true;
+}
 }
