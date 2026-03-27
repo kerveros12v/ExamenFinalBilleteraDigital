cf8f0d072ec86a4d97015306187540746eaa3e59
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-03-26 20:08:16 -0500

    feat: Implementacion del constructor con el atributo exitosa en true

--- BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
+++ BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
@@ -3,4 +3,11 @@ public class Transaccion {
     private double monto;
     private String descripcion;
     private boolean exitosa;
+
+    public Transaccion(String tipo, double monto, String descripcion) {
+        this.tipo = tipo;
+        this.monto = monto;
+        this.descripcion = descripcion;
+        this.exitosa = true;
+    }
 }
