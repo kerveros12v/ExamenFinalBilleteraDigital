b2824dc9ba4a1b26d57075e6a982b17362c335d2
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-03-26 20:39:16 -0500

    feat: Implementacion del metodo marcarFallida() del tipo void

--- BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
+++ BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
@@ -60,12 +60,20 @@ public class Transaccion {
 
     public void aplicarComision(double porcentaje) {
         double comision = monto*(porcentaje/100);
-        double montoTotal = monto+comision;
+        double montoTotal = monto + comision;
 
         System.out.println("Comisión (" + porcentaje + "%): $" + comision);
         System.out.println("Monto total con comisión: $" + montoTotal);
         
-    }      
+    }  
+    
+    
+    public void marcarFallida() {
+        this.exitosa = false;
+        System.out.println("Transacción marcada como fallida.");
+        System.out.println("Monto afectado: $" + monto);
+    }
+
       
            
 }
