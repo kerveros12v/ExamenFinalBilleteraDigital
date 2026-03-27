e7a3c530d167be72370cc500a32145623ac02091
kerveros12v <kerveros12v@aol.com>
2026-03-26 19:57:25 -0500

    feal:add function mostrarInfo

--- BilleteraDigital/src/com/krakedev/Billetera/Billetera.java
+++ BilleteraDigital/src/com/krakedev/Billetera/Billetera.java
@@ -8,4 +8,11 @@ public Billetera(String propietario, String numeroCuenta, double saldoInicial){
     this.saldo=saldoInicial;
     this.activa=true;
 }
+public void mostrarInfo(){
+    System.out.println("=== Billetera Digital ===");
+    System.out.println("Propietario: " +propietario+
+                "\nCuenta: " + numeroCuenta+
+                "\nSaldo: " +saldo+
+                "\nEstado: "+((activa)?"Activa":"Inactiva"));
+}
 }
