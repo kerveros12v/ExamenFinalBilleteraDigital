c404b64efd5bdbd7cd189a09d3ec8551c80fc877
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-03-26 20:11:54 -0500

    feat: implementacion de getters y setters

--- BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
+++ BilleteraDigital/src/com/krakedev/Transaccion/Transaccion.java
@@ -10,4 +10,39 @@ public class Transaccion {
         this.descripcion = descripcion;
         this.exitosa = true;
     }
+
+
+    public String getTipo() {
+    return tipo;
+    }
+
+    public void setTipo(String tipo) {
+    this.tipo = tipo;
+    }
+
+    public double getMonto() {
+    return monto;
+    }
+
+    public void setMonto(double monto) {
+    this.monto = monto;
+    }
+
+    public String getDescripcion() {
+    return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+    }
+
+    public boolean isExitosa() {
+    return exitosa;
+    }
+
+    public void setExitosa(boolean exitosa) {
+    this.exitosa = exitosa;
+    }   
+
+    
 }
