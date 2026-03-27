368c018e8381cae0fa5804613907bf77844c1a52
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2026-03-26 20:03:56 -0500

    feat: add class Contacto, attributes and builder

---
+++ BilleteraDigital/src/com/krakedev/Contacto/Contacto.java
@@ -0,0 +1,15 @@
+public class Contacto {
+
+ private String nombre;
+ private String cuentaDestino;
+ private String banco;
+ private boolean favorito;
+ 
+ public Contacto (String nombre, String cuentaDestino, String banco) {
+	 this.nombre = nombre;
+	 this.cuentaDestino = cuentaDestino;
+	 this.banco = banco;
+	 this.favorito = false;
+ }
+ 
+}
